b2cf7f77a0f3e39c3982c3594bfa7b0ee41ebd4c
Allan Edgard Silva Freitas <allanedgard@gmail.com>
2017-07-02 15:51:06 -0700

    Some play with time

--- teste1.py
+++ teste1.py
@@ -1,13 +1,18 @@
 import Buffer
-
+import time
+y = int(time.time()*1000)
+time.sleep(.001)
 x = Buffer.Buffer()
-x.add(10, 'teste 1')
-x.add(10, 'teste 2')
-x.add(12, 'prova 3')
-x.add(14, 'prova 4')
-print x.getMsgs(14)
-x.add(16, 'teste 1')
-x.add(17, 'teste 2')
-x.add(18, 'prova 3')
-print x.getMsgs(18)
+x.add(int(time.time()*1000)%y, 'teste 1')
+x.add(int(time.time()*1000)%y, 'teste 2')
+time.sleep(.001)
+x.add(int(time.time()*1000)%y, 'prova 3')
+x.add(int(time.time()*1000)%y, 'prova 4')
+print x.getMsgs(int(time.time()*1000)%y)
+time.sleep(.001)
+x.add(int(time.time()*1000)%y, 'teste 1')
+x.add(int(time.time()*1000)%y, 'teste 2')
+time.sleep(.001)
+x.add(int(time.time()*1000)%y, 'prova 3')
+print x.getMsgs(int(time.time()*1000)%y)
 
